52b5db32b9f4f7d9d62b028c90a521bc0019eda4
G186512021 <218651@st.takushoku-u.ac.jp>
2022-05-19 12:17:50 +0900

    class4

--- practice-dom/dom-demo.js
+++ practice-dom/dom-demo.js
@@ -8,3 +8,18 @@ ul.insertAdjacentElement('beforeend', li)
 li = document.createElement('li');
 li.textContent = 'ヨーロッパ大陸';
 ul.insertAdjacentElement('beforeend', li);
+
+let h2sevenOcean = document.querySelector('h2');
+let oceans = [
+    '北極海',
+    '北大西洋',
+    '南大西洋',
+    'インド洋',
+    '北太平洋',
+    '南太平洋',
+    '南極海'
+  ]
+  ul = document.createElement('ul');
+  for (let o of oceans) {
+      console.log(oceans);
+  }
