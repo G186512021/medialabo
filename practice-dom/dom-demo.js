07c3b17cc15a563e34a5e51deb96f06f09d50f00
G186512021 <218651@st.takushoku-u.ac.jp>
2022-05-19 12:23:41 +0900

    class4

--- practice-dom/dom-demo.js
+++ practice-dom/dom-demo.js
@@ -21,5 +21,5 @@ let oceans = [
   ]
   ul = document.createElement('ul');
   for (let o of oceans) {
-      ul.insertAdjacentElement('beforeend',h2sevenOcean)
+      ul.insertAdjacentElement('afterend',h2sevenOcean)
   }
