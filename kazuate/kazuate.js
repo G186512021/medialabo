495815d186fbd455aa45a37efc4e6efac25e2427
G186512021 <218651@st.takushoku-u.ac.jp>
2022-05-19 14:51:38 +0900

    class4

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -8,9 +8,6 @@ let kaisu = 0;
 // 予想を4回実行する
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
 hantei();
-hantei();
-hantei();
-hantei();
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
@@ -28,24 +25,13 @@ function hantei() {
         console.log("正解です．おめでとう!");
     }
  }
- if (kaisu>3) {
-     console.log("答えは " + kotae + " でした．すでにゲームは終わっています");
- }
-/* for (let i = 1; i <= 3; i++) {
-     console.log(i+"回目の予想: "+yoso);
-     if (yoso<kotae) {
-         console.log("まちがい．答えはもっと大きいですよ");
-     } else if (yoso>kotae) {
-         console.log("まちがい．答えはもっと小さいですよ");
-     } else if (yoso==kotae) {
-         console.log("正解です．おめでとう!");
-         break;
-     }
- }
+}
+if (kaisu>3) {
+  console.log("答えは " + kotae + " でした．すでにゲームは終わっています");
 
- for (let j = 4; j <= yoso; j++) {
-     console.log("答えは " + kotae + " でした．すでにゲームは終わっています");
- }*/
+
+ let p = document.querySelector('p#result');
+ p.insertAdjacentElement('afterend', p);
  
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
