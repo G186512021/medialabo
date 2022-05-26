e0c24e49905a99cf5bdea8de71995251d1043d90
G186512021 <218651@st.takushoku-u.ac.jp>
2022-05-26 14:23:46 +0900

    class5

--- kazuate/kazuate.js
+++ kazuate/kazuate.js
@@ -28,8 +28,9 @@ function hantei() {
  pp2.insertAdjacentElement('beforeend',p2);
  pp.insertAdjacentElement('afterend',p);
  
- if (kaisu<=4 && kaisu != 0) {
+ if (kaisu<=3 && kaisu != 0) {
     p2.textContent = kaisu+'回目の予想: '+math+' ';
+    kaisu++;
     if (math<kotae) {
         p.textContent = 'まちがい．答えはもっと大きいですよ';
     } else if (math>kotae) {
@@ -37,14 +38,13 @@ function hantei() {
     } else if (math==kotae) {
         p.textContent ='正解です．おめでとう!';
     }
-    kaisu++;
 }
 
 if (kaisu == 0) {
     kaisu++;
     }
 
-if (kaisu>4) {
+if (kaisu>3) {
     p.textContent ='答えは ' + kotae + ' でした．すでにゲームは終わっています';
 
 
