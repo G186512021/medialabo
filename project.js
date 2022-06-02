4c95bb61c0ead39504ad2aa7cfadc975e0bb40cb
G186512021 <218651@st.takushoku-u.ac.jp>
2022-06-02 12:28:30 +0900

    class6

---
+++ project.js
@@ -0,0 +1,37 @@
+let aj = document.querySelector('#sendRequest');
+aj.addEventListener('click', sendRequest);
+
+// 通信開始処理
+function sendRequest() {
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/360630.json';
+
+  //通信開始
+  axios.get(url)
+   .then(showResult)
+   .catch(showError)
+   .then(finish);
+}
+
+//成功処理
+function showResult(resp) {
+
+  // サーバから送られてきたデータを出力
+  let data = resp.data;
+
+  // data が文字列型なら，オブジェクトに変換する
+  if (typeof data === 'String') {
+    data = JSON.parse(data);
+  }
+}
+
+// dataを出力
+p.textContent = [data]
+// 通信エラーが発生した時の処理
+function showError(err) {
+console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+console.log('Ajax 通信が終わりました');
+}
