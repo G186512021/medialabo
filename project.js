8b0182fd596e0a2c244015e16bf5fef6617a6b05
G186512021 <218651@st.takushoku-u.ac.jp>
2022-06-02 14:16:47 +0900

    class6

--- project.js
+++ project.js
@@ -1,39 +1,42 @@
-let aj = document.querySelector('#sendRequest');
-aj.addEventListener('click', sendRequest);
+let b = document.querySelector('#sendRequest');
+b.addEventListener('click', sendRequest);
 
-// 通信開始処理
-let k = document.querySelector('Kairo');
-Kairo = k.value;
+
+// 通信を開始する処理
 function sendRequest() {
-  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+Kairo+'.json';
+	// URL を設定
+	let url = 'https://www.nishita-lab.org/web-contents/jsons/test.json';
 
-  //通信開始
-  axios.get(url)
-   .then(showResult)
-   .catch(showError)
-   .then(finish);
+	// 通信開始
+	axios.get(url)
+		.then(showResult)
+		.catch(showError)
+		.then(finish);
 }
 
-//成功処理
+// 通信が成功した時の処理
 function showResult(resp) {
+	// サーバから送られてきたデータを出力
+	let data = resp.data;
 
-  // サーバから送られてきたデータを出力
-  let data = resp.data;
+	// data が文字列型なら，オブジェクトに変換する
+	if (typeof data === 'string') {
+		data = JSON.parse(data);
+	}
 
-  // data が文字列型なら，オブジェクトに変換する
-  if (typeof data === 'String') {
-    data = JSON.parse(data);
-  }
+	// data をコンソールに出力
+	console.log(data);
+
+	// data.x を出力
+	console.log(data.x);
 }
 
-// dataを出力
-p.textContent = [data]
 // 通信エラーが発生した時の処理
 function showError(err) {
-console.log(err);
-}
+	console.log(err);
+}	
 
 // 通信の最後にいつも実行する処理
 function finish() {
-console.log('Ajax 通信が終わりました');
+	console.log('Ajax 通信が終わりました');
 }
