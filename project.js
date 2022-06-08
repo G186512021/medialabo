ea95b2fb0c31cf147f1d66fde40124b4e621b36a
G186512021 <218651@st.takushoku-u.ac.jp>
2022-06-08 22:25:17 +0900

    h

--- project.js
+++ project.js
@@ -35,6 +35,14 @@ function showResult(resp) {
 		if (b.checked) {
 			console.log(b.value);
 			p.textContent = [
+				data.coord.lon,
+				data.coord.lat,
+				data.weather.description,
+				data.main.temp_min,
+				data.main.temp_max,
+				data.main.humidity,
+				data.wind.speed,
+				data.wind.deg,
 				data.name,
 			]
 
