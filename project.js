a5c39c2366179e779ee108b70a559bd0b6da6574
G186512021 <218651@st.takushoku-u.ac.jp>
2022-07-07 03:40:46 +0900

    homework

--- project.js
+++ project.js
@@ -36,49 +36,49 @@ function showResult(resp) {
 		p.textContent = [data.coord.lon,]
 	}
 
-	let tex2 = document.querySelector('id#lat');
+	let tex2 = document.querySelector('input[id="lat"]');
 	tex2.addEventListener('click',addtext2);
 	function addtext2() {
 		p.textContent = [data.coord.lat,]
 	}
 
-	let tex3 = document.querySelector('id#weather');
+	let tex3 = document.querySelector('input[id="weather"]');
 	tex3.addEventListener('click',addtext3);
 	function addtext3() {
 		p.textContent = [data.weather[0].description,]
 	}
 
-	let tex4 = document.querySelector('id#temp_min');
+	let tex4 = document.querySelector('input[id="temp_min"]');
 	tex4.addEventListener('click',addtext4);
 	function addtext4() {
 		p.textContent = [data.main.temp_min,]
 	}
 
-	let tex5 = document.querySelector('id#temp_max');
+	let tex5 = document.querySelector('input[id="temp_max"]');
 	tex5.addEventListener('click',addtext5);
 	function addtext5() {
 		p.textContent = [data.main.temp_max,]
 	}
 
-	let tex6 = document.querySelector('id#humidity');
+	let tex6 = document.querySelector('input[id="humidity"]');
 	tex6.addEventListener('click',addtext6);
 	function addtext6() {
 		p.textContent = [data.main.humidity,]
 	}
 
-	let tex7 = document.querySelector('id#speed');
+	let tex7 = document.querySelector('input[id="speed"]');
 	tex7.addEventListener('click',addtext7);
 	function addtext7() {
 		p.textContent = [data.wind.speed,]
 	}
 
-	let tex8 = document.querySelector('id#deg');
+	let tex8 = document.querySelector('input[id="deg"]');
 	tex8.addEventListener('click',addtext8);
 	function addtext8() {
 		p.textContent = [data.wind.deg,]
 	}
 
-	let tex9 = document.querySelector('id#name');
+	let tex9 = document.querySelector('input[id="name"]');
 	tex9.addEventListener('click',addtext9);
 	function addtext9() {
 		p.textContent = [data.name,]
