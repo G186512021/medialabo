79d265d5c96581f29b79db050557ea349db92364
G186512021 <218651@st.takushoku-u.ac.jp>
2022-07-19 23:42:03 +0900

    homework

--- test.js
+++ test.js
@@ -11,61 +11,60 @@ function sendRequest() {
             console.log(r.value);
 			geturl = r.value;
         }
+	}
+	let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+geturl+'.json';
 
-		let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+geturl+'.json';
 
+	// 通信開始
+	axios.get(url)
+		.then(showResult)
+		.catch(showError)
+		.then(finish);
 
-		// 通信開始
-		axios.get(url)
-			.then(showResult)
-			.catch(showError)
-			.then(finish);
-	
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+	// サーバから送られてきたデータを出力
+	let data = resp.data;
+
+	// data が文字列型なら，オブジェクトに変換する
+	if (typeof data === 'string') {
+		data = JSON.parse(data);
+	}
+
+	let p;
+    p = document.createElement('p');
+	let aja = document.querySelector('div#placeholder');
+	aja.insertAdjacentElement('afterend', p);
+
+	let tex = document.querySelector('input[name="kensaku"]');
+	tex.addEventListener('click',addtext);
+	function addtext() {
+		p.textContent = [data.coord.lon,
+			data.coord.lat,
+			data.weather[0].description,
+			data.main.temp_min,
+			data.main.temp_max,
+			data.main.humidity,
+			data.wind.speed,
+			data.wind.deg,
+			data.name,]
 	}
 	
-	// 通信が成功した時の処理
-	function showResult(resp) {
-		// サーバから送られてきたデータを出力
-		let data = resp.data;
-	
-		// data が文字列型なら，オブジェクトに変換する
-		if (typeof data === 'string') {
-			data = JSON.parse(data);
-		}
-	
-		let p;
-		p = document.createElement('p');
-		let aja = document.querySelector('div#placeholder');
-		aja.insertAdjacentElement('afterend', p);
-	
-		rs.addEventListener('click',addtext);
-		function addtext() {
-			p.textContent = [data.coord.lon,
-				data.coord.lat,
-				data.weather[0].description,
-				data.main.temp_min,
-				data.main.temp_max,
-				data.main.humidity,
-				data.wind.speed,
-				data.wind.deg,
-				data.name,]
-		}
-		
-		// data をコンソールに出力
-		console.log(data);
-	
-		// data.x を出力
-		console.log(data.x);
-	}
-	
-	// 通信エラーが発生した時の処理
-	function showError(err) {
-		console.log(err);
-	}	
-	
-	// 通信の最後にいつも実行する処理
-	function finish() {
-		console.log('Ajax 通信が終わりました');
-	}
-		
+	// data をコンソールに出力
+	console.log(data);
+
+	// data.x を出力
+	console.log(data.x);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+	console.log(err);
+}	
+
+// 通信の最後にいつも実行する処理
+function finish() {
+	console.log('Ajax 通信が終わりました');
 }
